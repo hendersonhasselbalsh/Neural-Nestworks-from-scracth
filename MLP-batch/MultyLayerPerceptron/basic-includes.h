d1d56aa095557f85e8405349de47ce4a6dc109d0
HendersonHasselbalsh <wassabi.sashimi@gmail.com>
2024-11-16 12:41:27 -0300

    startting MLP project

---
+++ MLP-batch/MultyLayerPerceptron/basic-includes.h
@@ -0,0 +1,30 @@
+#pragma once
+
+////---------------------------------
+//// comunly used libs
+////---------------------------------
+
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <functional>
+#include <sstream>
+#include <vector>
+#include <filesystem>
+#include <random>
+#include <type_traits>
+#include <cassert>
+#include <fstream>
+#include <utility>
+#include <numeric>
+#include <ranges>
+#include <cfloat>
+#include <chrono>
+
+////---------------------------------
+//// linear algebra libs
+////---------------------------------
+
+#include <Eigen/Core>
+#include <Eigen/Dense>
---
+++ MLP-batch/MultyLayerPerceptron/main-project/main.cpp
@@ -0,0 +1,8 @@
+#include "../basic-includes.h"
+
+
+int main(int argc, const char** argv)
+{
+	std::cout << "hello world!!!\n";
+	return 0;
+}
